ca8467b4089cd507a086d23eb337ab5bde62b50e
Shahaadi <shahaadi285@gmail.com>
2026-07-08 13:42:51 +0530

    Refactor benchmarking example to use DeviceBuffer copy methods

--- examples/phase1/benchmarking.cu
+++ examples/phase1/benchmarking.cu
@@ -57,16 +57,8 @@ int main()
         example::DeviceBuffer<float> device_b(host_b.size());
         example::DeviceBuffer<float> device_c(host_c.size());
 
-        CUDADL_CUDA_CHECK(cudaMemcpy(
-            device_a.get(),
-            host_a.data(),
-            device_a.bytes(),
-            cudaMemcpyHostToDevice));
-        CUDADL_CUDA_CHECK(cudaMemcpy(
-            device_b.get(),
-            host_b.data(),
-            device_b.bytes(),
-            cudaMemcpyHostToDevice));
+        device_a.copy_from_host(host_a.data(), host_a.size());
+        device_b.copy_from_host(host_b.data(), host_b.size());
 
         for (int iteration = 0; iteration < warmup_iterations; ++iteration) {
             vector_add_kernel<<<blocks_per_grid, threads_per_block>>>(
@@ -95,11 +87,7 @@ int main()
 
         CUDADL_CUDA_CHECK_LAST_KERNEL("vector_add_kernel benchmark");
 
-        CUDADL_CUDA_CHECK(cudaMemcpy(
-            host_c.data(),
-            device_c.get(),
-            device_c.bytes(),
-            cudaMemcpyDeviceToHost));
+        device_c.copy_to_host(host_c.data(), host_c.size());
 
         float max_absolute_error = 0.0F;
         int mismatch_count = 0;
